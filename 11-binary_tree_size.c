97dcff14cb7ef79e59d610ff4c3b4ae420a976c7
Mutai05 <kelvok2014@gmail.com>
2024-02-01 05:47:20 -0800

    Task 11 - Fixed betty coding style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,10 +8,10 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    /* Base case: If the tree is NULL, return 0 */
-    if (tree == NULL)
-        return (0);
+	/* Base case: If the tree is NULL, return 0 */
+	if (tree == NULL)
+		return (0);
 
-    /* Recursively calculate the size of the left and right subtrees */
-    return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+	/* Recursively calculate the size of the left and right subtrees */
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
