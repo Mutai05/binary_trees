bdab171e9af8d78040bffd45ed5b2476a1b0259c
Mutai05 <kelvok2014@gmail.com>
2024-02-01 07:37:06 -0800

    Fixed betty coding style

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,7 +2,8 @@
 #include "stddef.h"
 
 /**
- * binary_tree_levelorder - Goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - Goes through a binary tree
+ * using level-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  *
@@ -10,25 +11,25 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    binary_tree_t *queue[10000] = {0};
-    int front = 0, rear = 0;
-    const binary_tree_t *current = NULL;
+	binary_tree_t *queue[10000] = {0};
+	int front = 0, rear = 0;
+	const binary_tree_t *current = NULL;
 
-    if (!tree || !func)
-        return;
+	if (!tree || !func)
+		return;
 
-    queue[rear++] = (binary_tree_t *)tree;
+	queue[rear++] = (binary_tree_t *)tree;
 
-    while (front < rear)
-    {
-        current = queue[front++];
+	while (front < rear)
+	{
+		current = queue[front++];
 
-        if (current->left)
-            queue[rear++] = (binary_tree_t *)current->left;
+		if (current->left)
+			queue[rear++] = (binary_tree_t *)current->left;
 
-        if (current->right)
-            queue[rear++] = (binary_tree_t *)current->right;
+		if (current->right)
+			queue[rear++] = (binary_tree_t *)current->right;
 
-        func(current->n);
-    }
+		func(current->n);
+	}
 }
