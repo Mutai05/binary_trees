e5f8a230df955a5b01d3c700fb10f254cbe18628
Mutai05 <kelvok2014@gmail.com>
2024-02-01 12:29:30 -0800

    Task 27 - Write a function that searches for a value in a Binary Search Tree

---
+++ 113-bst_search.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - searches for a value in a Binary Search Tree
+ * @tree: pointer to the root node of the BST to search
+ * @value: the value to search in the tree
+ * Return: A pointer to the node containing a value equals to `value`,
+ *         or `NULL` if the value is not found or if the tree is NULL
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL || tree->n == value)
+		return ((bst_t *)tree);
+
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
+}
