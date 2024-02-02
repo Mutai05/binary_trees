3e79cb7e772cd38345c2f1aff94620d4ff3f5c8c
Mutai05 <kelvok2014@gmail.com>
2024-02-02 13:07:31 -0800

    Fixed Aunty Betty

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -8,17 +8,17 @@
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left && tree->left->n > tree->n)
-        return 0;
+	if (tree->left && tree->left->n > tree->n)
+		return (0);
 
-    if (tree->right && tree->right->n > tree->n)
-        return 0;
+	if (tree->right && tree->right->n > tree->n)
+		return (0);
 
-    if (!binary_tree_is_heap(tree->left) || !binary_tree_is_heap(tree->right))
-        return 0;
+	if (!binary_tree_is_heap(tree->left) || !binary_tree_is_heap(tree->right))
+		return (0);
 
-    return 1;
+	return (1);
 }
